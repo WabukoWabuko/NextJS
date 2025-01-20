2bdbbdcaa68a1357d4542e0f24acf6d75de193a9
WabukoWabuko <basilwabbs@gmail.com>
2025-01-20 10:40:43 +0000

    Moving from page to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>Starting NextJS and TS</h1>
+      <Link href={"/about"}>About</Link>
     </main>
   );
 }
