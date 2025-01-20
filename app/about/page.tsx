1b50d558d9de959f9dafc3a06f2ca187921169e5
WabukoWabuko <basilwabbs@gmail.com>
2025-01-20 10:49:10 +0000

    Navigation between pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function About() {
-  return <h1>This is an About page</h1>;
+  return (
+    <>
+      <h1>This is an About page</h1>
+      <Link href={"/"}>Home</Link>
+    </>
+  );
 }
