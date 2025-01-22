2cb60c6d4f7004e70719bb3e05e0e498213becde
WabukoWabuko <basilwabbs@gmail.com>
2025-01-22 20:39:42 +0000

    Using folders in brackets for grouping of folders and files

---
+++ app/(Grouping)/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>About Page</h1>
+      <Link href={"/"}>Index Page</Link>
+    </main>
+  );
+}
