2beda68e30a98517f4bb03cf464293b37d9d6140
WabukoWabuko <basilwabbs@gmail.com>
2025-01-23 19:45:39 +0000

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Starting NextJS and TS</h1>
+      <h1>Starting NextJS and TS.</h1>
       <Link href={"/about"}>About Page</Link>
     </main>
   );
